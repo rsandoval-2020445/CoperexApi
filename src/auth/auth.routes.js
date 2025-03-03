89be280386aa9dc3e44e5ceba3ac0ad71734f776
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-03-02 19:11:25 -0600

    Añadidas validaciones de datos en las rutas de autenticación

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express"
 import { registerUser, loginUser } from "./auth.controller.js"
-import { registerValidator, loginValidator } from "../../helpers/validators.js" // Puedes agregar validaciones para los campos
-
+import { registerValidator, loginValidator } from "../../helpers/validators.js" 
 const api = Router()
 
 // Rutas de autenticación
-api.post("/register", registerValidator, registerUser) // Validación en el body para el registro
-api.post("/login", loginValidator, loginUser) // Validación en el body para login
+api.post("/register", registerValidator, registerUser) 
+api.post("/login", loginValidator, loginUser) 
 
 export default api
