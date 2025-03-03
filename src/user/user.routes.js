cdb4d80cfcefc920a224f4cf9fc2a65e5d5a31fe
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-03-02 19:12:32 -0600

    Actualizadas las rutas de usuario para agregar registro y login

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { createUser, loginUser } from "./user.controller.js"
+import { registerValidator, loginValidator } from "../../helpers/validators.js" // Importar las validaciones
 
 const router = Router()
 
 // Rutas de autenticación
-router.post("/register", createUser)  
-router.post("/login", loginUser)     
+router.post("/register", registerValidator, createUser)  
+router.post("/login", loginValidator, loginUser)         
 
 export default router
